41ea024f2f9dc89e3c01ac67d6cb668421bd7af8
Brogy-alt <brogangys2@gmail.com>
2022-11-07 16:22:30 +0200

    yeahh

---
+++ js/code.js
@@ -0,0 +1,22 @@
+let userName = 'Brogan';
+let userAge = '20';
+let isCool = 'true';
+
+// document.write(userName,"<br>");
+// document.write(userAge,"<br>");
+// document.write(isCool, "<br>");
+// document.write(`Name:${userName}<br>Age:${userAge}<br>Male:${isCool}`);
+
+// let name = 'Meezie outta here'
+// let age = 19
+// let isMale = true
+
+// document.write(name)
+// document.write("<br>"+ age)
+// document.write("<br>" + isMale)
+
+// Array    Array   Array    Array
+
+let names = ['Daniel', 'Mishaine', 'Matthew'];
+console.log
+
